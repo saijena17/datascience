bf6e3948d47b99dd29b369cef8630bb12cc9e626
saijena17 <33216986+saijena17@users.noreply.github.com>
2023-03-25 15:36:24 +0530

    Add files via upload

--- ML/Tbilisi_Housing_Challenge/model_app.py
+++ ML/Tbilisi_Housing_Challenge/model_app.py
@@ -8,18 +8,27 @@ app = Flask(__name__)
 filename = '/Users/saijena/Desktop/datascience/ML/Tbilisi_Housing_Challenge/model.pkl'
 model = pickle.load(open(filename, 'rb'))
 
-
+#as soon as we hit http://127.0.0.1:5000/ this is displayed
 @app.route("/")
 def home():
     return(render_template('index.html'))
 
+#as soon as we fill details and hit Predict button, it will call /predict answer is displayed
 @app.route('/predict', methods=['POST'])
 def predict():
     '''
     For rendering results on HTML GUI
     '''
-    print("Estimated Price: " + str(model.predict([27.0, 2.0, 3.0, 1.0, 50, 30])))
-    answer = "Estimated Price: " + str(model.predict([27.0, 2.0, 3.0, 1.0, 50, 30]))
+    #generator
+    if request.method == 'POST':
+        #fetching a particular column value
+        space = request.form['space']
+        print(space)
+        print(request.form.values())
+        #getting values from generator in form of string i.e   i is str here and we can convert it int(i)
+        print([i for i in request.form.values()])
+        print("Estimated Price: " + str(model.predict([int(i) for i in request.form.values()])))
+        answer = "Estimated Price: " + str(model.predict([int(i) for i in request.form.values()]))
     return(render_template('index.html', prediction_text = answer))
 
 
